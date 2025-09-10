792d093bb100a5f492471e8c08e3c923a220b49a
Niconavarrov <nicolas.navarro@iteso.mx>
2025-09-10 16:26:58 -0600

    Inicio del FRONT

---
+++ app/views/scripts/home.js
@@ -0,0 +1,4 @@
+function toggleImagen() {
+    const contenedor = document.getElementById('contenedorImagen');
+    contenedor.style.display = contenedor.style.display === 'none' ? 'block' : 'none';
+}
